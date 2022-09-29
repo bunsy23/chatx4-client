9b6d87a6f2c8f86114ec73e3d6a719771365d135
Hiromasy <bunsy2014@gmail.com>
2022-09-29 10:59:36 +0700

    connect socket with credential

--- src/utils/context/SocketContext.tsx
+++ src/utils/context/SocketContext.tsx
@@ -1,11 +1,8 @@
 import { createContext } from "react";
 import { io } from "socket.io-client";
 
-console.log(
-  "import.meta.env.VITE_WEBSOCKET_URL",
-  import.meta.env.VITE_WEBSOCKET_URL
-);
-
-export const socket = io(import.meta.env.VITE_WEBSOCKET_URL!);
+export const socket = io(import.meta.env.VITE_WEBSOCKET_URL!, {
+  withCredentials: true,
+});
 
 export const SocketContext = createContext(socket);
